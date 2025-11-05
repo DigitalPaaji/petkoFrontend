2232286cf4cc0b6ce768d65f6d92b6326408490b
vivek <vivek@vivek.vivek>
2025-11-05 12:16:41 +0530

    add

--- app/admin/components/apis.js
+++ app/admin/components/apis.js
@@ -1,8 +1,8 @@
 // export const baseurl ="http://localhost:8000/v1";
 // export const imgurl ="http://localhost:8000";
 
-export const baseurl = "https://zerobugs.cloud/v1"
-export const imgurl = "https://zerobugs.cloud/"
+export const baseurl = "https://api.zerobugs.cloud/v1"
+export const imgurl = "https://api.zerobugs.cloud/"
 
 
 
